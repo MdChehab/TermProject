3225c5a1d46b95b4945dd7ff8e084ddc998864eb
MdChehab <m.chehab777@gmail.com>
2025-12-01 03:22:04 -0500

    l

--- TermProject2025/obj/Debug/net8.0-windows/TermProject2025.AssemblyInfo.cs
+++ TermProject2025/obj/Debug/net8.0-windows/TermProject2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TermProject2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75864adcbe8c507f887af43ce69fcff8f91b865b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5df88fe9d808beece1caa17c921217b457a24dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TermProject2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TermProject2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
